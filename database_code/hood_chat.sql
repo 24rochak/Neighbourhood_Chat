f8b27d74fd2ebbd047e92d2955741572f1597041
Rochak <24rochak@gmail.com>
2020-01-04 02:50:49 -0500

    Update hood_chat.sql

--- database_code/hood_chat.sql
+++ database_code/hood_chat.sql
@@ -13,19 +13,19 @@ CREATE TABLE `hood_chat`.`user` (
   `uemail` VARCHAR(45) NULL,
   `upassword` VARCHAR(45) NULL,
   `uprofile` VARCHAR(45) NULL,
-  `uphoto` VARCHAR(45) NULL,
+  `uphoto` VARCHAR(200) NULL,
   PRIMARY KEY (`uid`, `uname`));
 
 insert into `user`(`uid`, `uname`, `ufullname`, `uaddress`, `uemail`, `upassword`, `uprofile`, `uphoto`) values
-(1, 'Ares', 'Zuoyiwei Zhang', '', 'aczzyw@gmail.com', 'pwd1', 'A handsome boy', 'None'),
-(2, 'Chuck', 'Rochak Agrawal', '', '24rochak@gmail.com', 'pwd2', 'A handsome boy too', 'None'),
-(3, 'Ricky', 'Boqi Tan', '', 'ricky@gmail.com', 'pwd3', '', 'None'),
-(4, 'Bob', 'Bobby Aloupis', '', 'bob@gmail.com', 'pwd4', '', 'None'),
-(5, 'Amy', 'Amy Suel', '', 'amy@gmail.com', 'pwd5', '', 'None'),
-(6, 'Tony', 'Tony Stark', '', 'tony123@gmail.com', 'pwd6', '', 'None'),
-(7, 'Babala', 'Crise Potter', '', 'cp1997@gmail.com', 'pwd7', '', 'None'),
-(8, 'Struke', 'Gary Lee', '', 'gary666@gmail.com', 'pwd8', '', 'None'),
-(9, 'Doglike', 'Anna Mellon', '', 'annapretty@gmail.com', 'pwd9', '', 'None');
+(1, 'Ares', 'Zuoyiwei Zhang', '', 'aczzyw@gmail.com', 'cHdkMQ==', 'A handsome boy', 'https://ptetutorials.com/images/user-profile.png'),
+(2, 'Chuck', 'Rochak Agrawal', '', '24rochak@gmail.com', 'cHdkMg==', 'A handsome boy too', 'https://ptetutorials.com/images/user-profile.png'),
+(3, 'Ricky', 'Boqi Tan', '', 'ricky@gmail.com', 'cHdkMw==', '', 'https://ptetutorials.com/images/user-profile.png'),
+(4, 'Bob', 'Bobby Aloupis', '', 'bob@gmail.com', 'cHdkNA==', '', 'https://ptetutorials.com/images/user-profile.png'),
+(5, 'Amy', 'Amy Suel', '', 'amy@gmail.com', 'cHdkNQ==', '', 'https://ptetutorials.com/images/user-profile.png'),
+(6, 'Tony', 'Tony Stark', '', 'tony123@gmail.com', 'cHdkNg==', '', 'https://ptetutorials.com/images/user-profile.png'),
+(7, 'Babala', 'Crise Potter', '', 'cp1997@gmail.com', 'cHdkNw==', '', 'https://ptetutorials.com/images/user-profile.png'),
+(8, 'Struke', 'Gary Lee', '', 'gary666@gmail.com', 'cHdkOA==', '', 'https://ptetutorials.com/images/user-profile.png'),
+(9, 'Doglike', 'Anna Mellon', '', 'annapretty@gmail.com', 'cHdkOQ==', '', 'https://ptetutorials.com/images/user-profile.png');
 
 
 /* 2. hood table */
@@ -88,7 +88,7 @@ insert into `message`(`mid`, `uid`, `mtitle`, `mtimestamp`, `mtext`, `mx`, `my`)
 (6, 2, 'Where can I wash clothes', '2019-11-25 03:00:00', 'Someone knows?', 0, 0),
 (7, 2, 'Rent a car', '2019-11-25 03:00:00', 'I want to rent a car and I will pay $1000 for a day. Someone has?', 0, 0),
 (8, 2, 'About car accident', '2019-11-25 06:00:00', 'Where is the bicycle accident happenned?', 0, 0),
-(9, 2, 'About car accident', '2019-11-25 06:00:00', 'Really? Sounds terrible!', 0, 0),
+(9, 3, 'About car accident', '2019-11-25 07:00:00', 'Really? Sounds terrible!', 0, 0),
 (10, 2, 'Basketball match', '2019-11-25 09:00:00', 'Does anyone like playing basketball?', 0, 0);
 
 
@@ -159,10 +159,10 @@ CREATE TABLE `hood_chat`.`applies` (
   `bid` INT NOT NULL,
   `acount` INT NOT NULL,
   PRIMARY KEY (`uid`, `bid`));
-
+/*
 insert into `applies`(`uid`, `bid`, `acount`) values
 (1, 1, 0);
-
+*/
 
 /* 10. ins table */
 DROP TABLE IF EXISTS `ins`;
@@ -228,7 +228,8 @@ CREATE TABLE `hood_chat`.`neighbors` (
     ON UPDATE NO ACTION);
 
 insert into `neighbors`(`uid`, `nid`) values
-(2, 4);
+(2, 4),
+(4, 2);
 
 /* 14. posts table */
 DROP TABLE IF EXISTS `posts`;
@@ -369,3 +370,118 @@ insert into `specifies`(`tid`, `uid`) values
 (4, 5),
 (4, 6),
 (4, 7);
+
+/*something added*/
+drop procedure if exists friend_location;
+delimiter $$
+create procedure friend_location
+	(in userid int)
+    begin
+	drop table if exists user_friends ;
+	create table user_friends as 
+		select f1.fid
+		from friends as f1, friends as f2
+		where f1.uid = f2.fid and f1.fid = f2.uid and f1.uid = userid;
+
+	select user_friends.fid,ux,uy,user.uname,user.uphoto from user_friends join user on user.uid=user_friends.fid;
+	drop table user_friends;
+    end;
+    $$
+delimiter ;
+
+drop procedure if exists neighbour_location;
+delimiter $$
+create procedure neighbour_location
+	(in userid int)
+    begin
+	drop table if exists user_neighbors;
+	create table user_neighbors as 
+		select nid from neighbors where uid = userid;
+        
+	select user_neighbors.nid,ux,uy,user.uname,user.uphoto from user_neighbors join user on user.uid=user_neighbors.nid;
+	drop table user_neighbors;
+    end;
+    $$
+delimiter ;
+
+drop procedure if exists block_locations;
+delimiter $$
+create procedure block_locations
+	(in userid int)
+	begin
+    drop table if exists hood_blocks;
+    create table hood_blocks as (select bid from belongs where hid = (select hid from belongs where bid = (select bid from ins where uid=userid)));
+    select block.bid,bsouthx,bsouthy,bnorthx,bnorthy from block join hood_blocks on hood_blocks.bid=block.bid;
+    drop table hood_blocks;
+    end;
+    $$
+    delimiter ;
+
+drop procedure if exists hood_location;
+delimiter $$
+create procedure hood_location
+	(in userid int)
+	begin
+    drop table if exists hood_id;
+    create table hood_id as (select hid from belongs where bid=(select bid from ins where uid=userid));
+    select hood.hid,hsouthx,hsouthy,hnorthx,hnorthy from hood join hood_id on hood_id.hid=hood.hid;
+    drop table hood_id;
+    end;
+    $$
+    delimiter ;	
+
+alter table user
+add column(ux double);
+
+alter table user
+add column(uy double);
+
+update user set ux = 40.637362, uy=-74.032494 where uid=2;
+update user set ux = 40.6360470, uy=-74.0317462 where uid=3;
+update user set ux = 40.635829,uy=-74.033557 where uid=4;
+
+update user set ux = 40.6320671,uy = -74.0256347 where uid =5;
+update user set ux = 40.634003, uy = -74.023018 where uid =6;
+update user set ux = 40.634231, uy = -74.020389 where uid =7;
+
+update user set ux = 40.672565, uy = -73.981554 where uid=8;
+update user set ux = 40.6722327,uy = -73.9842935 where uid = 9;
+
+alter table block
+modify column bsouthx double,modify column bsouthy double,modify column bnorthx double,modify column bnorthy double;
+update block set bsouthx = 40.635429,bsouthy = -74.034971, bnorthx = 40.638271, bnorthy = -74.030840 where bid=1;
+update block set bsouthx = 40.631507,bsouthy = -74.0277529, bnorthx = 40.633427,bnorthy = -74.021047 where bid=2;
+
+alter table hood
+modify column hsouthx double,modify column hsouthy double,modify column hnorthx double,modify column hnorthy double;
+update hood set hsouthx = 40.615977,hsouthy =  -74.042782, hnorthx = 40.638389, hnorthy =  -74.013777 where hid=1;
+
+
+alter table message
+modify column mx double, modify column my double;
+
+update message set mx = 40.632914, my= -74.029403 where mid = 1;
+update message set mx = 40.638320, my= -74.026313 where mid = 2;
+update message set mx = 40.638252, my= -74.019233 where mid = 3;
+update message set mx = 40.631174, my= -74.035730 where mid = 4;
+update message set mx = 40.632820, my= -74.017872 where mid = 5;
+update message set mx = 40.638016, my= -74.028584 where mid = 6;
+update message set mx = 40.629627, my= -74.028047 where mid = 7;
+update message set mx = 40.634922, my= -74.027409 where mid = 8;
+update message set mx = 40.671827, my= -73.975917 where mid = 9;
+update message set mx = 40.672245, my= -73.981794 where mid = 10;
+
+drop procedure if exists get_requests;
+	delimiter $$
+create procedure get_requests(
+	in usrid int)
+    begin
+    drop table if exists temp;
+    create table temp as (with `requests` as (SELECT uid FROM hood_chat.friends where fid=usrid) select * from requests where requests.uid not in(SELECT fid FROM hood_chat.friends where uid=usrid));
+    select user.uid, user.uemail, user.uname, user.ufullname from temp join user on user.uid=temp.uid;
+	drop table temp;
+    end;
+    $$
+    delimiter ;
+    
+/*call get_requests(2);*/
